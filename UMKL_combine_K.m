3a6d6dea25f03f44d65cbe6196c5706bc15ab204
jcc <707590291@qq.com>
2020-01-02 20:14:47 +0800

    update UMKL_combine_K.m

--- UMKL_combine_K.m
+++ UMKL_combine_K.m
@@ -1,5 +1,5 @@
-lamda_1 = 0;
-lamda_2 = 0;
+lamda_1 = [0.1,0.01,0.001];
+lamda_2 = [0.1,0.01,0.001];
 k1_paths = {['data2/kernels/disease_simmat_overlap.txt'],...
             ['data2/kernels/disease_simmat_semantic.txt'],...
             };
@@ -17,9 +17,24 @@ for i=1:length(k2_paths)
     K2_list(:,:,i) = load(k2_paths{i});
 end
 % 2. multiple kernel
-[weight_v1] = mas_UMKL_kernels_weights_norm(K1_list,lamda_1,lamda_2)
-K_COM1 = combine_kernels(weight_v1, K1_list);
-[weight_v2] = mas_UMKL_kernels_weights_norm(K2_list,lamda_1,lamda_2)
-K_COM2 = combine_kernels(weight_v2, K2_list);
-save_results('./data2/Kdiseases_UMKL_0505',K_COM1)
-save_results('./data2/Kgenes_UMKL_0505',K_COM2)
+for l1 = lamda_1
+    for l2 = lamda_2
+        [weight_v1] = mas_UMKL_kernels_weights_norm(K1_list,lamda_1,lamda_2)
+        K_COM1 = combine_kernels(weight_v1, K1_list);
+        [weight_v2] = mas_UMKL_kernels_weights_norm(K2_list,lamda_1,lamda_2)
+        K_COM2 = combine_kernels(weight_v2, K2_list);
+        w = [lamda_1,lamda_2,weight_v1,weight_v2]
+        fid=fopen('./weights.txt','at+'); %写的方式打开文件（若不存在，建立文件）
+        for i = 1:length(w)
+            fprintf(fid,'%f ',w(i));  % %d 
+            if i == length(w)
+                fprintf(fid,'\n');  % %d 
+            else
+                fprintf(fid,',');
+            end
+        end
+        fclose(fid);  %关闭文件；
+    end
+end
+% save_results('./data2/Kdiseases_UMKL_0505',K_COM1)
+% save_results('./data2/Kgenes_UMKL_0505',K_COM2)
